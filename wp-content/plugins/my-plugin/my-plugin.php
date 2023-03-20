466d7481bb61db3445ecd9749596cc088b04fe49
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-03-20 22:09:23 +0530

    first commit

---
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: My Plugin
+ * Description: This is test plugin
+ * version: 1.0
+ * Author: Dhruvik
+ */
+
+ if(!defined('ABSPATH')){
+    header("Location: /first_plugin");
+    die();
+ }
+ echo 'hello there!';
+?>
