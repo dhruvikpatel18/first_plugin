a727555a93624876e8f57aa40059386cb9b99b73
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-03-20 22:39:46 +0530

    define my-plugin

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -6,9 +6,32 @@
  * Author: Dhruvik
  */
 
+ //check if anyone access this page from url path then redirect to home
  if(!defined('ABSPATH')){
     header("Location: /first_plugin");
     die();
  }
- echo 'hello there!';
+ 
+ //activation hook
+ function my_plugin_activation(){
+    //
+ }
+ //__FILE__ provides the plugin's path
+ register_activation_hook(__FILE__,'my_plugin_activation');
+ 
+
+ //deactivation hook
+ function my_plugin_deactivation(){
+    //
+ }
+ register_deactivation_hook(__FILE__,'my_plugin_deactivation');
+
+ //uninstall hook is in separate file(uninstall.php)
+
+ //shortcode for backend wordpress pages
+ function my_sc_fun(){
+    return 'Function call';
+ }
+ add_shortcode('my-sc','my_sc_fun');
+
 ?>
---
+++ wp-content/plugins/my-plugin/uninstall.php
@@ -0,0 +1,6 @@
+<?php
+if(!defined('WP_UNINSTALL_PLUGIN')){
+    header("Location: /first_plugin");
+    die();
+}
+?>
